6a50603ef65c411e37dc21602595b61e9013cb0e
Yuki-Yui <yuukiyuinyan@gmail.com>
2025-01-29 23:08:07 +0900

    v2.0 x.comに対応

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ twitterの「いまなにしてる？」のところを書き換える拡張機�
 1. リポジトリをダウンロードするか ```git clone``` する
 1. twitter_placeholder.js を編集
     ```JavaScript:twitter_placeholder.js
-    let message = "ここに好きな文章を入力";
+    const message = "ここに好きな文章を入力";
     ```
-1. 拡張機能のページ(chrome://extensions/)に移動し、デベロッパーモードをオン
+1. [拡張機能のページ](chrome://extensions/)に移動し、デベロッパーモードをオン
 1. 画面上部の「パッケージ化されていない拡張機能を読み込む」をクリックし、cloneしたディレクトリを選択する
--- twitter_placeholder.js
+++ twitter_placeholder.js
@@ -1,13 +1,15 @@
-window.addEventListener("load",function(){
-    setInterval(change,500);
-})
+const message = "インターネットやめろ！！！";
+
 function change(){
-    let message = "インターネットやめろ！！！";
-    let ph = document.getElementsByClassName("public-DraftEditorPlaceholder-inner");
+    const ph = document.getElementsByClassName("public-DraftEditorPlaceholder-inner");
     if (ph.length != 0 && ph[0].innerHTML != message){
         for(let i = 0; i < ph.length; i++){
-            console.log("change");
             ph[i].innerHTML = message;
         }
     }
 }
+
+const observer = new MutationObserver(function(){
+    change();
+});
+observer.observe(document.body, {childList: true, subtree: true});
